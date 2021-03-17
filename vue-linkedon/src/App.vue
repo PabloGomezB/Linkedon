9d14cd0aa6356bbbbe04deb7c7e07b2be5fd26e8
a18jorcalari <a18jorcalari@inspedralbes.cat>
2021-03-17 13:18:42 +0100

    listar ofertas done

--- vue-linkedon/src/App.vue
+++ vue-linkedon/src/App.vue
@@ -1,28 +1,18 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+    <div id="app" class="container">
+        <UserView />
+    </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import UserView from "./components/UserView.vue";
 
 export default {
-  name: "App",
-  components: {
-    HelloWorld
-  }
+    name: "App",
+    components: {
+        UserView,
+    },
 };
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style lang="scss"></style>
---
+++ vue-linkedon/src/components/UserView.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <b-list-group>
+            <b-list-group-item
+                class="flex-column align-items-start"
+                v-for="oferta in resultadoOfertas"
+                :key="oferta.id"
+            >
+                <div class="d-flex w-100 justify-content-between">
+                    <h5 class="mb-1">{{ oferta.titol }}</h5>
+                    <small>{{ oferta.data_publicacio }}</small>
+                </div>
+                <p class="mb-1">
+                    {{ oferta.descripcio }}
+                </p>
+                <small>{{ oferta.ubicacio }}</small>
+            </b-list-group-item>
+        </b-list-group>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "UserView",
+    data() {
+        return {
+            resultadoOfertas: "",
+        };
+    },
+    methods: {
+        getOfertas() {
+            this.axios
+                .get(
+                    "http://labs.iam.cat/~a18jorcalari/Linkedon/api.php/records/oferta"
+                )
+                .then((response) => {
+                    console.log(response);
+                    this.resultadoOfertas = response.data.records;
+                });
+        },
+    },
+    created() {
+        this.getOfertas();
+    },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss"></style>
