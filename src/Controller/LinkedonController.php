ec012bb9a54a1c52bc2b573f1411b31de18be880
a18jorcalari <a18jorcalari@inspedralbes.cat>
2021-03-18 13:20:51 +0100

    vue incrustado

--- src/Controller/LinkedonController.php
+++ src/Controller/LinkedonController.php
@@ -6,11 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class LinkedonController extends AbstractController
-{
+class LinkedonController extends AbstractController {
     #[Route('/', name: 'linkedon')]
-    public function index(): Response
-    {
+    public function index(): Response {
         return $this->render('index.html.twig', [
             'controller_name' => 'LinkedonController',
         ]);
