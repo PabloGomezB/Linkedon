ee3f8a148aae63b6d8f13233f0d5d5bcb2e67d3a
PabloGomezB <73338310+PabloGomezB@users.noreply.github.com>
2021-03-22 19:32:17 +0100

    commit caca

--- src/Controller/OfertaController.php
+++ src/Controller/OfertaController.php
@@ -41,8 +41,6 @@ class OfertaController extends AbstractController
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($ofertum);
             $entityManager->flush();
-            // PILLAR CORREO
-            // echo '<script>console.log("'.$ofertum->getEmpresa()->getCorreu().'")</script>';
             return $this->redirectToRoute('linkedon_index');
         }
 
