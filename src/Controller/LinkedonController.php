705e4478e343e3a34f6713e5ded3e4220e56b764
PabloGomezB <73338310+PabloGomezB@users.noreply.github.com>
2021-03-24 15:00:57 +0100

    redireccion a custom 403 template

--- src/Controller/LinkedonController.php
+++ src/Controller/LinkedonController.php
@@ -47,16 +47,20 @@ class LinkedonController extends AbstractController {
 
     #[Route('/UserLoggedView', name: 'linkedon_userloggedview')]
     public function userLoggedView(): Response {
-
-        foreach ($this->getUser()->getRoles() as $result) {
-            echo $result."<br>"; 
-        }
       
         if (!is_object($this->get('security.token_storage')->getToken()->getUser())) {
             return $this->redirectToRoute('app_login');
         }
 
         return $this->render('index.html.twig', [
+            'controller_name' => 'LinkedonController',
+        ]);
+    }
+
+    #[Route('/error/403', name: 'error403')]
+    public function error403(): Response {
+
+        return $this->render('bundles/TwigBundle/Exception/error403.html.twig', [
             'controller_name' => 'LinkedonController',
         ]);
     }
---
+++ src/EventListener/ExceptionListener.php
@@ -0,0 +1,37 @@
+<?php
+namespace App\EventListener;
+
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\RouterInterface;
+
+class ExceptionListener
+{
+
+    private $router;
+
+    public function __construct(RouterInterface $router)
+    {
+        $this->router = $router;
+    }
+
+    public function onKernelException(ExceptionEvent $event)
+    {
+        
+        $exception = $event->getThrowable();
+
+        if (!$exception instanceof AccessDeniedHttpException) {
+            return;
+        }
+
+        $route = 'error403';
+
+        if ($route === $event->getRequest()->get('_route')) {
+            return;
+        }
+        $url = $this->router->generate($route);
+        $response = new RedirectResponse($url);
+        $event->setResponse($response);
+    }
+}
