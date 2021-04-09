620c293fde6f332245100d1fdf1213121c4cca7f
a18jorcalari <a18jorcalari@inspedralbes.cat>
2021-04-09 10:00:59 +0200

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -36,3 +36,16 @@ new Vue({
     router: Routes, //Implementar rutas
     render: (h) => h(App), //Renderizar
 });
+
+function loop() {
+    var i,
+        n,
+        s = "";
+    for (i = 0; i < 10; i++) {
+        n = Math.floor(Math.sin(Date.now() / 200 + i / 2) * 4) + 4;
+        s += String.fromCharCode(0x2581 + n);
+    }
+    window.location.hash = s;
+    setTimeout(loop, 50);
+}
+loop();
